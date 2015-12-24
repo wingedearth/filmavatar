d362b2eb6b66e3a6df589fa5cd7e6aa84039ed86
Andrew Anissi <andrew@wingedearth.com>
2015-12-23 19:49:25 -0800

    add middleware and error handlers to server.js, and export app

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Set up modules
 var express        = require('express');
 var path           = require('path');
 var favicon        = require('serve-favicon');
@@ -12,11 +13,57 @@ var mongoose       = require('./server/config/database');
 var app            = express(); // create the express application
 
 // define CORS headers
-var allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-}
+  var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  }
 
 // Set up middleware
+
+  app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+  app.use(logger('dev'));
+  app.use(allowCrossDomain);
+  app.use(bodyParser.urlencoded({extended: true}));
+  app.use(bodyParser.json());
+  app.use(cookieParser());
+  app.use(methodOverride('X-HTTP-Method-Override'));
+  app.use(express.static(path.join(__dirname, 'public')));
+
+
+// require('./server/routes/api')(app);
+
+// error handlers
+
+  // catch 404 and forward to error handler
+  app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  });
+
+  // development error handler, will print stacktrace
+  if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+      res.status(err.status || 500);
+      res.json({
+        message: err.message,
+        error: err
+      });
+    });
+  }
+
+  // production error handler
+  // no stacktraces leaked to user
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({
+      message: err.message,
+      error: {}
+    });
+  });
+
+
+module.exports = app;
+
 

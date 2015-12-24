48f10c7c116d4a3fc80bc4d1883ae22bc01691c1
Andrew Anissi <andrew@wingedearth.com>
2015-12-23 22:21:27 -0800

    add methods to CORS support

--- server.js
+++ server.js
@@ -19,6 +19,7 @@
 // define CORS headers
   var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
   }

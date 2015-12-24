c598ed096fd2d9470b0de8191386198fe0447154
Andrew Anissi <andrew@wingedearth.com>
2015-12-23 20:06:34 -0800

    move SECRET_KEY to env (and change it)

--- server/config/environment.js
+++ server/config/environment.js
@@ -5,7 +5,6 @@ require('dotenv').load();
 var localEnvVars = {
   TITLE:      'filmavatar',
   SAFE_TITLE: 'filmavatar',
-  SECRET_KEY: 'dowhatthouwiltshallbethewholeofthelaw'
 }
 
 module.exports = _.extend(process.env, localEnvVars);

a660a84ac8e630be8b502da9eda21396190adeb1
Andrew Anissi <andrew@wingedearth.com>
2015-12-31 21:48:36 -0800

    create user.dataservice.js to link front-end data with server api routes

--- client/js/app/services/user.dataservice.js
+++ client/js/app/services/user.dataservice.js
@@ -10,8 +10,41 @@
   function userDataService($http) {
 
     var userFactory = {};
-
     userFactory.user = {};
+
+    // GET a user
+    userFactory.get = function(id) {
+      return $http.get('/api/users/' + id);
+    };
+
+    // GET current user's own info
+    userFactory.get = function(id) {
+      return $http.get('/api/me/');
+    };
+
+    // GET all users
+    userFactory.all = function() {
+      return $http.get('/api/users/');
+    };
+
+    // POST (create) a new user
+      // userData is an object that includes the following keys:
+      // email, handle, city, state, zip, password (all as Strings)
+    userFactory.create = function(userData) {
+      return $http.post('/api/users/', userData);
+    };
+
+    // PUT (edit/update) a user
+      // userData is an object that includes the following keys:
+      // email, handle, city, state, zip, password (all as Strings)
+    userFactory.update = function(id, userData) {
+      return $http.put('/api/users/' + id, userData);
+    };
+
+    // delete a user
+    userFactory.delete = function(id) {
+      return $http.delete('/api/users/' + id);
+    };
 
     return userFactory;
   }

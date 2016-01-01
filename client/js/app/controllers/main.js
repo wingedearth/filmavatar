1c9dc65de9373920301c749e0eeb90fcfa80101d
Andrew Anissi <andrew@wingedearth.com>
2015-12-31 21:05:08 -0800

    add  to MainController

--- client/js/app/controllers/main.js
+++ client/js/app/controllers/main.js
@@ -8,11 +8,13 @@
   MainController.$inject = ["$scope", "$state", "$log"];
   // MainController.$inject = ["$scope", "$state", "userDataService", "$log", "authService"];
 
-  function MainController($scope, $state, userDataService, $log, authService) {
-    var vm = this;
-    vm.videos = "video1";
-    vm.$state = $state;
-
+  // function MainController($scope, $state, userDataService, $log, authService) {
+  function MainController($scope, $state, userDataService, $log) {
+    // var vm = this;
+    // vm.videos = "video1";
+    // vm.$state = $state;
+    $scope.$state = $state;
+    $scope.videos = "video2";
   }
 
 })();
